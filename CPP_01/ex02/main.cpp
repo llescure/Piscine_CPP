b0dc9f8b0cd9ced8c86d28337d52183585335936
Lea LESCURE <llescure@e3r11p8.clusters.42paris.fr>
2022-05-17 09:43:57 +0200

    CPP01: ex02 done

--- CPP_01/ex02/main.cpp
+++ CPP_01/ex02/main.cpp
@@ -4,8 +4,17 @@
 int main(void)
 {
     std::string str;
-    stringPTR = *str;
-    stringREF = &str;
+    std::string *stringPTR = &str;
+    std::string &stringREF = str;
 
     str = "HI THIS IS BRAIN";
+    // Print the address
+    std::cout << &str << std::endl;
+    std::cout << stringPTR << std::endl;
+    std::cout << &stringREF << std::endl;
+
+    // Print the value
+    std::cout << str << std::endl;
+    std::cout << *stringPTR << std::endl;
+    std::cout << stringREF << std::endl;
 }
