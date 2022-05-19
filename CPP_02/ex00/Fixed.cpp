4b853f944a1b10423cdb9c0f4dffc5cc421d59b3
Lea LESCURE <llescure@e1r9p7.clusters.42paris.fr>
2022-05-19 11:28:38 +0200

    CPP_02: ex00

--- CPP_02/ex00/Fixed.cpp
+++ CPP_02/ex00/Fixed.cpp
@@ -1,22 +1,47 @@
 #include "Fixed.hpp"
 
-Fixed::Fixed(void) _fixed_number(0)
+Fixed::Fixed(void) :_fixed_number(0)
 {
+    std::cout << "Default constructor called" << std::endl;
     return  ;
+}
+
+Fixed::Fixed(Fixed const &src)
+{
+    std::cout << "Copy constructor called" << std::endl;
+
+    *this = src;
+    return ;
 }
 
 Fixed::~Fixed(void)
 {
+    std::cout << "Destructor called" << std::endl;
     return ;
 }
 
-int     getRawBits(void)
+Fixed &   Fixed::operator=(Fixed const & rhs)
 {
+    std::cout << "Copy assignment operator called" << std::endl; 
+
+    if (this != &rhs)
+    {
+        this->_fixed_number = rhs.getRawBits();
+    }
+
+    return *this;
+}
+
+int     Fixed::getRawBits(void) const
+{
+    std::cout << "getRawBits member function called" << std::endl;
     return (this->_fixed_number);
 }
 
-void    setRawBits(int const raw)
+void    Fixed::setRawBits(int const raw)
 {
+    std::cout << "setRawBits member function called" << std::endl;
+    
     this->_fixed_number = raw;
     return ;
 }
--- CPP_02/ex00/Fixed.hpp
+++ CPP_02/ex00/Fixed.hpp
@@ -9,14 +9,15 @@ class Fixed
     Fixed(void);
     ~Fixed(void);
     Fixed(Fixed const & src);
-    Fixed &operator=(const &Fixed);
 
-    int     getRawBits(void);
+    Fixed &operator=(Fixed const & rhs);
+
+    int     getRawBits(void) const;
     void    setRawBits(int const raw);
 
     private:
     int _fixed_number;
     static int const _bits_number;
-;}
+};
 
 #endif
