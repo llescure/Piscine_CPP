15c655451457e03754b75b2787216b343abb8400
Lea LESCURE <llescure@e3r10p19.clusters.42paris.fr>
2022-05-21 15:26:39 +0200

    CPP_03: ex02

--- CPP_03/ex02/main.cpp
+++ CPP_03/ex02/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include "ClapTrap.hpp"
 #include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
 int main(void)
 {
-    ClapTrap pikatchu("Pikatchu");
-    ScavTrap rondoudou("Rondoudou");
+    ClapTrap    pikatchu("Pikatchu");
+    ScavTrap    rondoudou("Rondoudou");
+    FragTrap    salameche("Salameche");
     
     std::cout << std::endl;
     pikatchu.setAttackDamage(2);
@@ -16,8 +18,15 @@ int main(void)
     std::cout << std::endl;
     rondoudou.attack("Pikatchu");
     pikatchu.takeDamage(rondoudou.getAttackDamage());
-    pikatchu.attack("Rondoudou");
     rondoudou.guardGate();
+
+    std::cout << std::endl;
+    salameche.attack("Rondoudou");
+    rondoudou.takeDamage(salameche.getAttackDamage());
+    rondoudou.attack("Salameche");
+    salameche.takeDamage(rondoudou.getAttackDamage());
+    salameche.beRepaired(1);
+    salameche.highFivesGuys();
     std::cout << std::endl;
 
     return (0);
