13f5315543da3a0348c2712c846e251085333cfa
Lea LESCURE <llescure@e1r10p9.42.fr>
2022-06-08 12:30:30 +0200

    CPP_06: ready to push

--- CPP_06/ex02/main.cpp
+++ CPP_06/ex02/main.cpp
@@ -1,93 +1,88 @@
 #include "Base.hpp"
 #include <cstdlib>
+#include <exception>
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
 
-Base    *generate(void)
+Base *generate(void)
 {
-    int     random;
-    Base    *newBase;
+    int random;
+    Base *newBase;
 
     srand(time(NULL));
     random = rand() % 3 + 1;
     switch (random)
     {
-        case 1:
+    case 1:
         newBase = new A;
         break;
-        case 2:
+    case 2:
         newBase = new B;
         break;
-        case 3:
+    case 3:
         newBase = new C;
         break;
     }
     return (newBase);
 }
 
-void    identify(Base *p)
+void identify(Base *p)
 {
-    A   *a = NULL;
-    B   *b = NULL;
-    C   *c = NULL;
-    try
-    {
-        a = dynamic_cast<A *>(p);
-        b = dynamic_cast<B *>(p);
-        c = dynamic_cast<C *>(p);
+    std::cout << "Call identity(Base *p):" << std::endl;
+    A *a = NULL;
+    B *b = NULL;
+    C *c = NULL;
+    a = dynamic_cast<A *>(p);
+    b = dynamic_cast<B *>(p);
+    c = dynamic_cast<C *>(p);
 
-        if (a != NULL)
-        {
-            std::cout << "The object pointed by the pointer is of type A" << std::endl;
-        }
-        if (b != NULL)
-        {
-            std::cout << "The object pointed by the pointer is of type B" << std::endl;
-        }
-        if (c != NULL)
-        {
-            std::cout << "The object pointed by the pointer is of type C" << std::endl;
-        }
-    }
-    catch(const std::bad_cast& e) 
+    if (a != NULL)
     {
-        std::cerr << e.what() << '\n';
-    }
-    
-}
-
-void    identify(Base &p)
-{
-    try
-    {
-        A  &a = dynamic_cast<A &>(p);
-        (void) a;
         std::cout << "The object pointed by the pointer is of type A" << std::endl;
     }
-    catch(const std::bad_cast& e) 
+    if (b != NULL)
     {
-        std::cerr << e.what() << '\n';
+        std::cout << "The object pointed by the pointer is of type B" << std::endl;
+    }
+    if (c != NULL)
+    {
+        std::cout << "The object pointed by the pointer is of type C" << std::endl;
+    }
+}
+
+void identify(Base &p)
+{
+    std::cout << "Call identity(Base &p):" << std::endl;
+    try
+    {
+        A &a = dynamic_cast<A &>(p);
+        (void)a;
+        std::cout << "The object pointed by the pointer is of type A" << std::endl;
+    }
+    catch (const std::exception &e)
+    {
+        (void)e;
     }
     try
     {
         B &b = dynamic_cast<B &>(p);
-        (void) b;
+        (void)b;
         std::cout << "The object pointed by the pointer is of type B" << std::endl;
     }
-    catch(const std::bad_cast& e) 
+    catch (const std::exception &e)
     {
-        std::cerr << e.what() << '\n';
+        (void)e;
     }
     try
     {
         C &c = dynamic_cast<C &>(p);
-        (void) c;
+        (void)c;
         std::cout << "The object pointed by the pointer is of type C" << std::endl;
     }
-    catch(const std::bad_cast& e) 
+    catch (const std::exception &e)
     {
-        std::cerr << e.what() << '\n';
+        (void)e;
     }
 }
 
@@ -97,6 +92,8 @@ int main(void)
 
     test = generate();
     identify(test);
-    identify(test);
+    std::cout << std::endl;
+    identify(*test);
+    std::cout << std::endl;
     delete test;
 }
